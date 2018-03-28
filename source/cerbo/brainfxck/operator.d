b3581d252f7a90636bf663533aeb2195245948a5
masaniwa <masaniwasdp@gmail.com>
2018-03-28 23:17:13 +0900

    Modifis la basan tipon de OperatorType.

--- source/cerbo/brainfxck/operator.d
+++ source/cerbo/brainfxck/operator.d
@@ -1,6 +1,9 @@
 module cerbo.brainfxck.operator;
 
-package enum OperatorType { pInc, pDec, vInc, vDec, vPut, vGet, pJmp, pEnd };
+package enum OperatorType : ubyte
+{
+    pInc, pDec, vInc, vDec, vPut, vGet, pJmp, pEnd
+};
 
 package struct Operator
 {
