76509573cb2ab38912b782d3ab32aa73aa7ddf06
masaniwa <masaniwasdp@gmail.com>
2018-04-21 00:54:40 +0900

    Aldonis maxPointer.

--- source/cerbo/brainfxck/memory.d
+++ source/cerbo/brainfxck/memory.d
@@ -4,11 +4,13 @@ import cerbo.brainfxck.exception : MemoryOutException, OverflowException;
 
 version (unittest) import fluent.asserts;
 
+package alias DataType = ubyte;
+
 package class Memory
 {
     void next(in uint time) pure @safe
     {
-        if (memorySize - 1 - pointer_ < time)
+        if (maxPointer - pointer_ < time)
         {
             throw new MemoryOutException(`The pointer pointed out of memory.`);
         }
@@ -28,7 +30,7 @@ package class Memory
 
     void inc(in uint time) pure @safe
     {
-        if (ubyte.max - buffer_[pointer_] < time)
+        if (DataType.max - buffer_[pointer_] < time)
         {
             throw new OverflowException(`The value overflowed.`);
         }
@@ -46,24 +48,26 @@ package class Memory
         buffer_[pointer_] -= time;
     }
 
-    void store(in ubyte value) nothrow @nogc pure @safe
+    void store(in DataType value) nothrow @nogc pure @safe
     {
         buffer_[pointer_] = value;
     }
 
-    ubyte load() const nothrow @nogc pure @safe
+    DataType load() const nothrow @nogc pure @safe
     {
         return buffer_[pointer_];
     }
 
     private size_t pointer_;
 
-    private ubyte[memorySize] buffer_;
+    private DataType[memorySize] buffer_;
 
     invariant
     {
-        assert(pointer_ < memorySize);
+        assert(pointer_ <= maxPointer);
     }
 }
 
 private enum memorySize = 0x01u << 0x0Fu;
+
+private enum maxPointer = memorySize - 1;
